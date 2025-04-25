40bbaafd2fb63f75f242e3cbb73cd8427044ffe0
Marcoouf <marcouf.lebar@gmail.com>
2025-04-25 08:35:03 +0200

    Push du code sans les images (images déjà en ligne)

--- src/data/oeuvres.ts
+++ src/data/oeuvres.ts
@@ -2,31 +2,31 @@ export const oeuvres = [
     {
       id: "1",
       titre: "Lueur abstraite",
-      image: "/images/oeuvre1.png",
+      image: "/images/oeuvre1.webp",
       artiste: "Artiste A",
     },
     {
       id: "2",
       titre: "Défragmentation",
-      image: "/images/oeuvre2.png",
+      image: "/images/oeuvre2.webp",
       artiste: "Artiste B",
     },
     {
       id: "3",
       titre: "Nuances éthérées",
-      image: "/images/oeuvre3.png",
+      image: "/images/oeuvre3.webp",
       artiste: "Artiste C",
     },
     {
         id: "4",
         titre: "Plage Quinéville",
-        image: "/images/oeuvre4.png",
+        image: "/images/oeuvre4.webp",
         artiste: "Artiste D",
       },
       {
         id: "5",
         titre: "Test5",
-        image: "/images/oeuvre5.png",
+        image: "/images/oeuvre5.webp",
         artiste: "Artiste A",
       },
       
